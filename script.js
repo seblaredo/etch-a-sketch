4043b5bb2fadd975682bc8d57fa1b5d0106112b7
seblaredo <38118181+seblaredo@users.noreply.github.com>
2026-06-19 15:47:10 -0400

    Add opacity modifier and toggleable color mode
    Users can decide if they want color, opacity now imitates etch a sketch

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
+const button = document.querySelector("#size");
+const buttonColor = document.querySelector("#color")
+let colorMode = false;
 setGrid(16);
 container.addEventListener('mouseover', (e)=>{
     let red = Math.floor(Math.random()*255+1);
     let green = Math.floor(Math.random()*255+1);
     let blue = Math.floor(Math.random()*255+1);
     let child = e.target.closest(".square");
-    //child.classList.add("hover");
-    //child.style.backgroudColor = `rgb(${red}, ${green}, ${blue})`;
-    child.style.backgroundColor = "rgb(10, 0, 0)";
+    const styles = window.getComputedStyle(child);
+    if (colorMode){
+        child.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;   
+    } else {
+        child.style.backgroundColor = `rgb(0, 0, 0)`;
+    }
+    
+    child.style.opacity = Number(styles.opacity) + 0.1;
 })
-
-// container.addEventListener('mouseout', (e)=>{
-//     let child = e.target.closest(".square");
-//     child.classList.toggle("hover");
-// })
 
 button.addEventListener('click', (e)=>{
     let size = prompt("Input size of grid:");
     setGrid(size);
 })
+
+buttonColor.addEventListener('click', (e)=>{
+    if (colorMode) colorMode = false;
+    else if (!colorMode) colorMode = true;
+});
+
 function setGrid(size){
     if (size > 100 || size < 1){
         alert("Invalid! Use a number between 1 and 100");
