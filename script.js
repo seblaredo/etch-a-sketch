b73aec7dcf8ec65378dbd45a3568de54b7ceddcc
seblaredo <38118181+seblaredo@users.noreply.github.com>
2026-06-19 15:01:26 -0400

    Add hover behavior and custom grid size
    Grid size can now be changed accordind to need with a button

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const SIZE = 16;
 const container = document.querySelector(".container");
-let grid = [];
+const button = document.querySelector("button");
+setGrid(16);
+container.addEventListener('mouseover', (e)=>{
+    let child = e.target.closest(".square");
+    child.classList.toggle("hover");
+})
 
-for (let r = 0; r < SIZE; r++){
-    grid[r] = []
-    for (let c = 0; c < SIZE; c++){
-        grid[r][c] = document.createElement("div");
-        grid[r][c].classList.add("square");
-    }
-}
+container.addEventListener('mouseout', (e)=>{
+    let child = e.target.closest(".square");
+    child.classList.toggle("hover");
+})
 
-for (const row of grid){
-    for (const item of row){
-        container.appendChild(item);
+button.addEventListener('click', (e)=>{
+    let size = prompt("Input size of grid:");
+    setGrid(size);
+})
+function setGrid(size){
+    if (size > 100 || size < 1){
+        alert("Invalid! Use a number between 1 and 100");
     }
+    container.replaceChildren();
+    let grid = [];
+    for (let r = 0; r < size; r++){
+        grid[r] = []
+        for (let c = 0; c < size; c++){
+            grid[r][c] = document.createElement("div");
+            grid[r][c].classList.add("square");
+            grid[r][c].style.flexBasis = 100/size + "%";
+        }
+    }
+    
+    for (const row of grid){
+        for (const item of row){
+            container.appendChild(item);
+        }
+    }
+
 }
